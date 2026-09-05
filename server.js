9cc4d7944e8175a42a43d102ac4c7b3b53c966c0
RosaPos <rposadashernndez@byupathway.edu>
2026-09-04 20:39:23 -0600

    Build static Express routes and pages

--- server.js
+++ server.js
@@ -1,14 +1,44 @@
 import express from 'express';
+import { fileURLToPath } from 'url';
+import path from 'path';
 
 const NODE_ENV =
   process.env.NODE_ENV?.toLowerCase() || 'production';
 
 const PORT = process.env.PORT || 3000;
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
 
+/**
+ * Configure Express middleware
+ */
+
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+/**
+ * Routes
+ */
+
 app.get('/', (req, res) => {
-  res.send('Hello from CSE 340!');
+  res.sendFile(
+    path.join(__dirname, 'src/views/home.html')
+  );
+});
+
+app.get('/organizations', (req, res) => {
+  res.sendFile(
+    path.join(__dirname, 'src/views/organizations.html')
+  );
+});
+
+app.get('/projects', (req, res) => {
+  res.sendFile(
+    path.join(__dirname, 'src/views/projects.html')
+  );
 });
 
 app.listen(PORT, () => {
